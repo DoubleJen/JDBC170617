793f605ffd8db8563d6ebdc51a42c08da261e06b
Mac <Mac@Mac-PC>
2017-06-17 14:01:28 +0800

    170617A001

---
+++ JDBC170617/src/tw/jdbc/JDBC170617A002.java
@@ -0,0 +1,56 @@
+package tw.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBC170617A002 {
+	
+	public static void main(String[] args){
+		//1. Driver
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+				//forName		
+				//"com.mysql.jdbc.Driver fr file:///C:/JDBC001/mysql-connector-java-5.1.42/docs/connector-j.html
+			System.out.println("OK");
+		}catch(ClassNotFoundException ee){
+			System.out.println(ee);
+			System.exit(-1);
+		}
+		
+		
+		//2. Build Connection
+		try{
+		Connection conn =
+				DriverManager.getConnection("jdbc:mysql://localhost/double", "root", "root");
+			//Connection --> java.sql
+			//"jdbc:mysql://localhost/test?" + "user=root&password=root" fr file:///C:/JDBC001/mysql-connector-java-5.1.42/docs/connector-j.html
+			//MySQL create one DB.
+		System.out.println("OK");
+		
+		//3. SQL Statement
+				Statement stmt = conn.createStatement();
+				
+		//4. query
+				String sql = "INSERT INTO cust(cname, tel, birthday)" +
+							 "VALUES('Tony','0963','1985-01-12')";
+				boolean isQueryOK = stmt.execute(sql);
+				if(isQueryOK){
+					System.out.println("OK");
+				}else{
+					System.out.println("XX");
+				}
+		
+		}catch(SQLException se){
+			System.out.println(se);
+			
+		}
+		
+		
+		
+		
+		
+	}
+
+}
