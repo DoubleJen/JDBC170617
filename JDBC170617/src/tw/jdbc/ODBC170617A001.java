5c031777b5b99e31e36198b3574333223f579990
Mac <Mac@Mac-PC>
2017-06-17 16:57:58 +0800

    170617A002

---
+++ JDBC170617/src/tw/jdbc/ODBC170617A001.java
@@ -0,0 +1,19 @@
+package tw.jdbc;
+
+public class ODBC170617A001 {
+
+	public static void main(String[] args) {
+		try{
+			//1. Driver
+			//Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+				// "sun.jdbc.odbc.JdbcOdbcDriver" fr http://www.herongyang.com/JDBC/JDBC-ODBC-DSN-Connection.html
+			//System.out.println("OK");
+			//java8 not support
+		}catch(Exception e){
+			System.out.println(e);
+			
+		}
+
+	}
+
+}
